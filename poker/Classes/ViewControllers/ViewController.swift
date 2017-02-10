2720358ac0eb4f60a0e74548363bac863fa40fc6
Kosuke <cellpromote-takahashi@co.kayac.com>
2017-02-10 10:00:35 +0900

    add project

--- poker/Classes/ViewControllers/ViewController.swift
+++ poker/Classes/ViewControllers/ViewController.swift
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // FeedBackツールです
+        // FeedBackツール
         Balto.show()
         
         self.cardCollectionView.allowsMultipleSelection = true
